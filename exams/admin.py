db5c2473470116d8737970218afe7bf4d582c8ef
MD Al Imran Khan <mdkha24@student.sdu.dk>
2026-06-19 18:20:42 +0200

    Change some admin

--- exams/admin.py
+++ exams/admin.py
@@ -4,6 +4,20 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Exam, ExamDetails, ExamQuestionAnswer
 
-admin.site.register(Exam)
-admin.site.register(ExamDetails)
-admin.site.register(ExamQuestionAnswer)
+#admin.site.register(Exam)
+@admin.register(Exam)
+class SystemSettings(admin.ModelAdmin):
+    list_display = ('id', 'exam_code', 'name', 'full_points', 'company_name', 'academic_year')  
+    #list_per_page = settings.PER_PAGE
+    
+#admin.site.register(ExamDetails)
+@admin.register(ExamDetails)
+class SystemSettings(admin.ModelAdmin):
+    list_display = ('id', 'exam__name', 'title_order', 'title', 'relation_to_question_no')  
+    list_filter = ('exam__name',)
+
+#admin.site.register(ExamQuestionAnswer)
+@admin.register(ExamQuestionAnswer)
+class SystemSettings(admin.ModelAdmin):
+    list_display = ('id', 'exam__name', 'question_serial', 'points')  
+    list_filter = ('exam__name', 'question_serial',)
--- grading/admin.py
+++ grading/admin.py
@@ -10,6 +10,7 @@ from .models import StudentAnswer, StudentGrade, StudentAnswerDetails, LlmLog
 class SystemSettings(admin.ModelAdmin):
     list_display = ('id', 'exam__name', 'student_name', 'total_point', 'grade')  
     #list_per_page = settings.PER_PAGE
+    list_filter = ('exam__name', 'student_name',)
 
     def has_add_permission(self, request):
         return False
@@ -25,6 +26,7 @@ class SystemSettings(admin.ModelAdmin):
 class SystemSettings(admin.ModelAdmin):
     list_display = ('id', 'exam__name', 'student_name', 'question_serial', 'llm_model', 'llm_score_points', 'llm_used_alternative_approach', 'llm_has_response', 'llm_response_in_sec')  
     #list_per_page = settings.PER_PAGE
+    list_filter = ('exam__name', 'student_name', 'question_serial',)
 
     def has_add_permission(self, request):
         return False
@@ -40,6 +42,7 @@ class SystemSettings(admin.ModelAdmin):
 class SystemSettings(admin.ModelAdmin):
     list_display = ('id', 'exam__name', 'student_answer__student_name', 'student_answer__question_serial', 'title', 'score', 'max_score', 'is_from_guideline')  
     #list_per_page = settings.PER_PAGE
+    list_filter = ('exam__name', 'student_answer__student_name', 'student_answer__question_serial',)
 
     def has_add_permission(self, request):
         return False
@@ -54,6 +57,7 @@ class SystemSettings(admin.ModelAdmin):
 class SystemSettings(admin.ModelAdmin):
     list_display = ('id', 'student_name', 'exam_id', 'question_serial', 'creation_date_time', 'llm_model', 'llm_response_in_sec', 'llm_response_total_duration_sec')  
     #list_per_page = settings.PER_PAGE
+    list_filter = ('exam_id', 'student_name', 'question_serial',)
 
     def has_add_permission(self, request):
         return False
